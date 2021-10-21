9669aebc1d6015036770ad6c779e95ffc2fe3082
joestack <joern@joern-online.de>
2021-10-21 17:36:08 +0200

    kk 6

--- templates/kk_userdata.sh
+++ templates/kk_userdata.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 apt-get update
-apt-get install -y openjdk-11-jdk unzip 
-curl https://github.com/keycloak/keycloak/releases/download/15.0.2/keycloak-15.0.2.zip
+apt-get install -y openjdk-8-jdk unzip 
+wget https://github.com/keycloak/keycloak/releases/download/15.0.2/keycloak-15.0.2.zip
+unzip keycloak-15.0.2.zip
+cd keycloak-15.0.2
+./bin/add-user-keycloak.sh -r master -u admin -p admin
+./bin/standalone.sh -b=0.0.0.0
