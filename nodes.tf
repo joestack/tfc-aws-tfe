8f211c0fbe5e5fe10a8a55eceda541296a0d91f2
joestack <joern@joern-online.de>
2021-10-21 17:52:37 +0200

    kk 7

--- nodes.tf
+++ nodes.tf
@@ -75,3 +75,28 @@ resource "aws_instance" "keycloak_node" {
 
   user_data = file("./templates/kk_userdata.sh")
 }
+
+
+resource "aws_security_group" "kk" {
+  name        = "${var.name}-kk-sg"
+  description = "private keycloak"
+  vpc_id      = aws_vpc.hashicorp_vpc.id
+}
+
+resource "aws_security_group_rule" "kk-http" {
+  security_group_id = aws_security_group.kk.id
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "kk-egress" {
+  security_group_id = aws_security_group.kk.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
--- templates/kk_userdata.sh
+++ templates/kk_userdata.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install -y openjdk-8-jdk unzip 
+cd /opt
 wget https://github.com/keycloak/keycloak/releases/download/15.0.2/keycloak-15.0.2.zip
 unzip keycloak-15.0.2.zip
 cd keycloak-15.0.2
